4af24e8d94372ab3cf7d3f7728664d8551094f3f
ALaah04 <alaahedhli45@gmail.com>
2026-06-26 04:29:11 +0100

    fix: remove submodules add real folders

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  updateStock,
+} = require('../controllers/productController');
+
+router.get('/', getProducts);
+router.get('/:id', getProduct);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+router.patch('/:id/stock', updateStock);
+
+module.exports = router;
